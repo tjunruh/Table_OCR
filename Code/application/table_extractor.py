c5e9f0f4eb288324cf6d78a63c65afea03aa63f9
tjunruh <tanner.unruh@gmail.com>
2023-06-27 20:41:28 -0500

    Organize table_extractor.py into functions

--- Code/application/table_extractor.py
+++ Code/application/table_extractor.py
@@ -3,43 +3,55 @@ import cv2
 import numpy as np
 import file_manager as fm
 
-def extract_cells(file_path):
+def pdf_to_jpg(file_path):
     jpgs = convert_from_path(file_path)
 
     for page in range(len(jpgs)):
         jpgs[page].save('page' + str(page) + '.jpg', 'JPEG')
 
+    return jpgs
+
+def rotate_jpgs(jpgs):
     for page in range(len(jpgs)):
         img = cv2.imread('page' + str(page) + '.jpg')
         img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    return img
 
+def prepare_binary_image(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_bin1 = 255-img_gray
-    thresh1, img_bin_otsu = cv2.threshold(img_bin1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    img_bin_otsu = cv2.threshold(img_bin1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    return img_bin_otsu
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 4))
-
+def get_vertical_lines(img_bin_otsu, img):
     vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, np.array(img).shape[1]//100))
-    eroded_image = cv2.erode(img_bin_otsu, vertical_kernel, iterations=3)
-    vertical_lines = cv2.dilate(eroded_image, vertical_kernel, iterations=3)
+    vertical_lines = cv2.erode(img_bin_otsu, vertical_kernel, iterations=3)
+    vertical_lines = cv2.dilate(vertical_lines, vertical_kernel, iterations=3)
+    return vertical_lines
 
-    hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (np.array(img).shape[1]//100, 1))
-    horizontal_lines = cv2.erode(img_bin_otsu, hor_kernel, iterations=5)
+def get_horizontal_lines(img_bin_otsu, img):
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (np.array(img).shape[1]//100, 1))
+    horizontal_lines = cv2.erode(img_bin_otsu, horizontal_kernel, iterations=5)
+    horizontal_lines = cv2.dilate(horizontal_lines, horizontal_kernel, iterations=5)
+    return horizontal_lines
 
-    horizontal_lines = cv2.dilate(horizontal_lines, hor_kernel, iterations=5)
-
+def get_vertical_horizontal_lines(vertical_lines, horizontal_lines):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 4))
     vertical_horizontal_lines = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
     vertical_horizontal_lines = cv2.erode(~vertical_horizontal_lines, kernel, iterations=3)
+    vertical_horizontal_lines = cv2.threshold(vertical_horizontal_lines, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    return vertical_horizontal_lines
 
-    thresh, vertical_horizontal_lines = cv2.threshold(vertical_horizontal_lines, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    contours, hierarchy = cv2.findContours(vertical_horizontal_lines, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+def get_boundingBoxes(vertical_horizontal_lines):
     boundingBoxes = []
+    contours = cv2.findContours(vertical_horizontal_lines, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
     for contour in contours:
         boundingBox = cv2.boundingRect(contour)
         if ((boundingBox[2]<1000) and (boundingBox[3]<500)):
             boundingBoxes.append(boundingBox)
+    return boundingBoxes
+
+def sort_boundingBoxes(boundingBoxes):
     rows_columns = []
     rows_columns = fm.load_rows_columns()
     columns = int(rows_columns[1])
@@ -51,16 +63,24 @@ def extract_cells(file_path):
         grabbed_section = boundingBoxes[int(start_position):int(start_position + columns)]
         grabbed_section = sorted(grabbed_section, key=lambda x:x[0])
         boxes.extend(grabbed_section)
-    
+    return boxes
+
+def save_boxes(boxes, img):
 
     for i, box in enumerate(boxes):
-
         x, y, w, h = box
-        print(str(x) + " , " + str(y))
         box_expand = 2
         roi = img[(y - box_expand):(y + h + box_expand), (x - box_expand):(x + w + box_expand)]
-
         filename = f'../../Storage/box_{i}.jpg'
         cv2.imwrite(filename, roi)
-    
-    
+        
+def extract_cells(file_path):
+   jpgs = pdf_to_jpg(file_path)
+   img = rotate_jpgs(jpgs)
+   img_bin_otsu = prepare_binary_image(img)
+   vertical_lines = get_vertical_lines(img_bin_otsu, img)
+   horizontal_lines = get_horizontal_lines(img_bin_otsu, img)
+   vertical_horizontal_lines = get_vertical_horizontal_lines(vertical_lines, horizontal_lines)
+   boundingBoxes = get_boundingBoxes(vertical_horizontal_lines)
+   boxes = sort_boundingBoxes(boundingBoxes)
+   save_boxes(boxes, img)

21758a29c65110de942885aaa8e7417989ad33e5
tjunruh <tanner.unruh@gmail.com>
2023-06-28 22:11:02 -0500

    Add character replacement to shorthand

--- Code/application/short_to_long.py
+++ Code/application/short_to_long.py
@@ -3,8 +3,19 @@ import file_manager as fm
 def short_to_long(predictions):
     shorthand = {}
     shorthand = fm.load_shorthand()
+    char_set = {}
+    string_set = {}
+    for key, value in shorthand.items():
+        if key.find('*') != -1:
+            char_set[key.replace('*', '')] = value
+        else:
+            string_set[key] = value
+
     for i in range(len(predictions)):
-        if predictions[i] in shorthand:
-            predictions[i] = shorthand[predictions[i]]
+        if predictions[i] in string_set:
+            predictions[i] = string_set[predictions[i]]
+        for key, value in char_set.items():
+            if predictions[i].find(key) != -1:
+                predictions[i] = predictions[i].replace(key, value)
 
     return predictions

dc006a456a06346408e1d6061d609084691ce055
tjunruh <tanner.unruh@gmail.com>
2023-06-30 23:52:12 -0500

    Add save and exit buttons

--- Code/application/edit_shorthand.py
+++ Code/application/edit_shorthand.py
@@ -2,11 +2,11 @@ import tkinter as tk
 from tkscrolledframe import ScrolledFrame
 import file_manager as fm
 
-global root, title_frame, button_frame, scroll_frame, edit_frame
+global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
 global rows, shorthand_list, shorthand
 
 def delete_entry(entry1, entry2, button1):
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, shorthand_list
     index = 0
     for e1, e2, b1 in shorthand_list:
@@ -20,7 +20,7 @@ def delete_entry(entry1, entry2, button1):
         index += 1
 
 def create_entry():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, shorthand_list
     row = rows
     entry1=tk.Entry(edit_frame, font=("Arial", 10), width=24)
@@ -34,7 +34,7 @@ def create_entry():
     rows += 1
 
 def initialize():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, shorthand_list, shorthand
     shorthand = {}
     shorthand_list = []
@@ -47,25 +47,28 @@ def initialize():
         shorthand_list[row][1].insert(0,long)
 
 def close():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
+    global shorthand, shorthand_list
+    root.grab_release()
+    root.destroy()
+
+def save():
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global shorthand, shorthand_list
     shorthand.clear()
     for e1, e2, b1 in shorthand_list:
         if((e1.get() != "") and (e2.get() != "")):
             shorthand[e1.get()] = e2.get()
     fm.save_shorthand(shorthand)
-    root.grab_release()
-    root.destroy()
-
 
 def run():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
-    root = tk.Toplevel()
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
+    root = tk.Tk()
     root.grab_set()
 
     root.title("Edit Shorthand")
-    root.rowconfigure(3, weight=1)
-    root.columnconfigure(2, weight=1)
+    root.rowconfigure(4, weight=1)
+    root.columnconfigure(1, weight=1)
 
     title_frame = tk.Frame(root)
     title_frame.rowconfigure(0, weight=1)
@@ -76,6 +79,11 @@ def run():
     button_frame.rowconfigure(0, weight=1)
     button_frame.columnconfigure(0, weight=1)
     button_frame.grid(row=2, column=0, sticky='nsew')
+
+    button_frame2 = tk.Frame(root)
+    button_frame2.rowconfigure(0, weight=1)
+    button_frame2.columnconfigure(1, weight=1)
+    button_frame2.grid(row=3, column=0, sticky='nsew')
 
     scroll_frame = ScrolledFrame(root)
     scroll_frame.grid(row=1, column=0, sticky='nsew')
@@ -88,7 +96,9 @@ def run():
     tk.Label(title_frame, text="     Shorthand     ", font=("Arial", 15)).grid(row=0, column=0, sticky='nsew')
     tk.Label(title_frame, text="    Expands To     ", font=("Arial", 15)).grid(row=0, column=1, sticky='nsew')
     tk.Label(title_frame, text="     ", font=("Arial", 15)).grid(row=0, column=2, sticky='nsew')
-    tk.Button(button_frame, text="Add Shorthand Entry", font=("Arial", 15), command=create_entry).grid(row=0, column=0, pady=10, padx=10) 
+    tk.Button(button_frame, text="Add Shorthand Entry", font=("Arial", 15), command=create_entry).grid(row=0, column=0, pady=10, padx=10)
+    tk.Button(button_frame2, text="       Save        ", font=("Arial", 15), command=save).grid(row=0, column=0, pady=10, padx=10)
+    tk.Button(button_frame2, text="       Exit        ", font=("Arial", 15), command=close).grid(row=0, column=1, pady=10, padx=10)
 
     initialize()
 
--- Code/application/ignore_rows.py
+++ Code/application/ignore_rows.py
@@ -2,11 +2,11 @@ import tkinter as tk
 from tkscrolledframe import ScrolledFrame
 import file_manager as fm
 
-global root, title_frame, button_frame, scroll_frame, edit_frame
+global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
 global rows, ignore_list, ignore
 
 def delete_entry(entry1, entry2, button1):
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, ignore_list
     index = 0
     for e1, e2, b1 in ignore_list:
@@ -20,7 +20,7 @@ def delete_entry(entry1, entry2, button1):
         index += 1
 
 def create_entry():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, ignore_list
     row = rows
     entry1=tk.Entry(edit_frame, font=("Arial", 10), width=24)
@@ -35,7 +35,7 @@ def create_entry():
 
 
 def initialize():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global rows, ignore_list, ignore
     ignore = {}
     ignore_list = []
@@ -48,26 +48,30 @@ def initialize():
         ignore_list[_row][1].insert(0, row)
 
 def close():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
+    global ignore, ignore_list
+    root.grab_release()
+    root.destroy()
+
+def save():
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     global ignore, ignore_list
     ignore.clear()
     for e1, e2, b1 in ignore_list:
         if(str(e1.get()).isnumeric() and str(e2.get()).isnumeric()):
             ignore.append([e1.get(), e2.get()])
     fm.save_ignore(ignore)
-    root.grab_release()
-    root.destroy()
 
     
 def run():
-    global root, title_frame, button_frame, scroll_frame, edit_frame
+    global root, title_frame, button_frame, scroll_frame, edit_frame, button_frame2
     root = tk.Tk()
 
     root.grab_set()
 
     root.title("Ignore Rows")
-    root.rowconfigure(3, weight=1)
-    root.columnconfigure(2, weight=1)
+    root.rowconfigure(4, weight=1)
+    root.columnconfigure(1, weight=1)
 
     title_frame = tk.Frame(root)
     title_frame.rowconfigure(0, weight=1)
@@ -79,6 +83,11 @@ def run():
     button_frame.columnconfigure(0, weight=1)
     button_frame.grid(row=2, column=0, sticky='nsew')
 
+    button_frame2 = tk.Frame(root)
+    button_frame2.rowconfigure(0, weight=1)
+    button_frame2.columnconfigure(1, weight=1)
+    button_frame2.grid(row=3, column=0, sticky='nsew')
+
     scroll_frame = ScrolledFrame(root)
     scroll_frame.grid(row=1, column=0, sticky='nsew')
     scroll_frame.bind_arrow_keys(root)
@@ -87,11 +96,13 @@ def run():
     edit_frame.rowconfigure(1, weight=1)
     edit_frame.columnconfigure(3, weight=1)
 
-    tk.Label(title_frame, text="     Page     ", font=("Arial", 15)).grid(row=0, column=0, sticky='nsew')
-    tk.Label(title_frame, text="     Row     ", font=("Arial", 15)).grid(row=0, column=1, sticky='nsew')
+    tk.Label(title_frame, text="        Page       ", font=("Arial", 15)).grid(row=0, column=0, sticky='nsew')
+    tk.Label(title_frame, text="        Row        ", font=("Arial", 15)).grid(row=0, column=1, sticky='nsew')
     tk.Label(title_frame, text="     ", font=("Arial", 15)).grid(row=0, column=2, sticky='nsew')
-    tk.Button(button_frame, text="Add Ignore Row Entry", font=("Arial", 15), command=create_entry).grid(row=2, column=0, pady=10, padx=10) 
-
+    tk.Button(button_frame, text="Add Ignore Row Entry", font=("Arial", 15), command=create_entry).grid(row=0, column=0, pady=10, padx=10) 
+    tk.Button(button_frame2, text="        Save       ", font=("Arial", 15), command=save).grid(row=0, column=0, pady=10, padx=10)
+    tk.Button(button_frame2, text="        Exit       ", font=("Arial", 15), command=close).grid(row=0, column=1, pady=10, padx=10)
+    
     initialize()
 
     root.protocol("WM_DELETE_WINDOW", close)

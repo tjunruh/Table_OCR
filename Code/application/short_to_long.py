b75c219b6626a91c54954aca891554e5906d51d0
tjunruh <tanner.unruh@gmail.com>
2023-08-15 14:59:09 -0500

    Add functionality to add multiple entries in a single cell while editing

--- Code/application/short_to_long.py
+++ Code/application/short_to_long.py
@@ -8,6 +8,7 @@ class short_to_long:
         shorthand = self.file_manager_operative.load_shorthand()
         char_set = {}
         string_set = {}
+        
         for key, value in shorthand.items():
             if key.find('*') != -1:
                 char_set[key.replace('*', '')] = value
@@ -17,6 +18,7 @@ class short_to_long:
         for i in range(len(predictions)):
             if predictions[i] in string_set:
                 predictions[i] = string_set[predictions[i]]
+                        
             for key, value in char_set.items():
                 if predictions[i].find(key) != -1:
                     predictions[i] = predictions[i].replace(key, value)
@@ -44,9 +46,22 @@ class short_to_long:
 
         if word in string_set:
             word = string_set[word]
+        elif word.find(',') != -1:
+            word = word.replace(' ', '')
+            words = []
+            words = word.split(',')
+            word = ''
+            for w in words:
+                if w in string_set:     
+                    w = string_set[w]
+                word = word + w + ', '
+            word = word[:len(word)-2]
+
         for key, value in char_set.items():
             if word.find(key) != -1:
                 word = word.replace(key, value)
 
         return word
+        
+    
         
